257f5538f81700d7337259e245f7e0f91f906835
SAHIB KHAN <sahibkhan9311@gmail.com>
2026-09-16 09:03:31 +0000

    No need

--- first programm.c
+++ first programm.c
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-#include <stdio.h> 
+#include <stdio.h>
 int main(){
-    printf("Hello world");
+    printf("Hello World");
     return 0;
 }
-=======
-#include <stdio.h> 
->>>>>>> 9199bec05d796bec0ea143a9830cb4a7718ba15f
-
-<<<<<<< HEAD
-=======
-int main(){
-    printf("Hello world");
-    return 0;
-}
-
->>>>>>> 9199bec05d796bec0ea143a9830cb4a7718ba15f
